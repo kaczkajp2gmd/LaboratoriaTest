aaa371198aabae51970eaccd6d7deb9a604aab22
Kaczka <adrian.kielbowicz99@gmail.com>
2019-10-12 00:04:04 +0200

    Main method updated

--- TestKonfliktu/src/newpackage/newclass.java
+++ TestKonfliktu/src/newpackage/newclass.java
@@ -4,6 +4,9 @@ public class newclass {
 	
 	static void Main(String args[])
 	{
-		System.out.println("JP2gmd");
+		System.out.println("Hello there.");
+		
+		for(int i = 0; i < 10; i++)
+		System.out.println(i);
 	}
 }
